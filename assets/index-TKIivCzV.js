9268a877bfe3aecba32e7ce9ae6c1b317d2b88c2
Abdulkhamid-dev <Abdulkhamid-dev@users.noreply.github.com>
2024-11-12 15:54:50 +0000

    deploy: ac852e1db328556f45d8bb218c075d5168b5cb70

--- assets/index-TKIivCzV.js
+++ assets/index-TKIivCzV.js
@@ -1,3 +1,3 @@
-import{s as e,j as t}from"./index-BhGBv2LJ.js";const r=e("div")`
+import{s as e,j as t}from"./index-BFxgnh_x.js";const r=e("div")`
   text-align: center;
 `,n=()=>t.jsx(r,{children:t.jsx("h1",{children:"Page Not Pound"})});export{n as default};
